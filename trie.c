a9124a3c439043c3da7b4b8665da73ded83511fc
Chan-Sung Kim <ck4135@cs.rit.edu>
2022-11-09 15:04:02 -0500

    project provided materials

---
+++ trie.c
@@ -0,0 +1,37 @@
+// 
+// File: trie.c 
+// Starter trie module for an integer-keyed trie ADT data type
+// @author CS@RIT.EDU
+// @author TODO add YOUR FULL NAME AND LOGIN HERE
+// // // // // // // // // // // // // // // // // // // // // // // // 
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "trie.h"
+
+/// Node_s: binary tree node TODO 
+/// Node is a pointer to a dynamically allocated struct Node_s.
+/// NodeH is a pointer to a pointer to a struct Node_s.
+
+typedef
+struct Node_s {
+    //TODO define Node_s structure content here
+} * Node;
+
+typedef Node * NodeH;          ///< handle to a trie node
+
+/// Trie_s: TODO define the Trie ADT here
+
+struct Trie_s {
+    /// Trie_s: TODO define the Trie ADT here
+};
+
+// global constants for bit operations and sizes
+// used by the application program
+
+//TODO implementation for constants
+
+//TODO implementation for everything after this
+
